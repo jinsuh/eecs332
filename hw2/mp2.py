e8f800341d2162afcac1d4dcae859fed3e644f6a
Suhong Jin <suhongjin94@gmail.com>
2014-10-12 17:21:12 -0500

    edited erosion

--- hw2/mp2.py
+++ hw2/mp2.py
@@ -17,13 +17,18 @@ def erosion(image, structure_element):
 			if pixel != 0:
 				isZero = False
 				for tup in flattened_elements:
-					if image.getpixel((col + tup[1], row + tup[0])) == 0:
-						isZero = True
-						finArr[row][col] = 0
-						break
+					#check bounds
+					if (inBounds(row, col, tup, width, height)):
+						if image.getpixel((col + tup[1], row + tup[0]))[0] == 255:
+							isZero = True
+							finArr[row][col] = 0
+							break
 				if (not isZero):
 					finArr[row][col] = 1
 	return finArr
+
+def inBounds(row, col, tup, width, height):
+	return col+tup[1] >= 0 and row+tup[0] >= 0 and col+tup[1] < width and row+tup[0] < height
 
 
 def dilation(image, structure_element):
@@ -49,11 +54,12 @@ def flatten_structure_element(structure_element):
 
 	return flattened_structure_element
 
-image = read_image('gun.bmp')
+
+image = read_image('test_erosion.bmp')
 structure_element = [[1, 1, 1], [1, 1, 1], [1, 1, 1]]
 # print flatten_structure_element(structure_element)
 finalArr = erosion(image, structure_element)
 # print finalArr
 numpyArr = (np.array(finalArr) * 255).astype(np.uint8)
 im = Image.fromarray(numpyArr)
-im.save('result.bmp')
+im.save('result_test.bmp')
