8e233c2ff9fcad56096b2f01d83e2358e01155dd
Suhong Jin <suhongjin94@gmail.com>
2014-10-14 21:52:00 -0500

    greg is a lazy f***

--- hw2/mp2_sj.py
+++ hw2/mp2_sj.py
@@ -1,32 +1,31 @@
 from PIL import Image
 import numpy as np
 
-def read_image(image_path):
-	image = Image.open(image_path)
-	image_data = image.getdata()
-	width, height = image_data.size
+def readImage(imagePath):
+	image = Image.open(imagePath)
+	imageData = image.getdata()
+	width, height = imageData.size
 
-	image_array = [[0 for x in xrange(width)] for x in xrange(height)]
+	imageArray = [[0 for x in xrange(width)] for x in xrange(height)]
 
 	for row in range(height):
 		for col in range(width):
-			image_array[row][col] = image_data.getpixel((col, row))[0]
+			imageArray[row][col] = imageData.getpixel((col, row))
 
-	return image_array
+	return imageArray
 
-def erosion(image, structure_element):
+def erosion(image, structureElement):
 	height = len(image)
 	width = len(image[0])
 
 	finArr = [[0 for x in xrange(width)] for x in xrange(height)]
-	flattened_elements= flatten_structure_element(structure_element)
+	flattenedElements= flattenStructureElement(structureElement)
 	for row in range(height):
 		for col in range(width):
 			pixel = image[row][col]
 			if pixel != 0:
 				isZero = False
-				for tup in flattened_elements:
-					#check bounds
+				for tup in flattenedElements:
 					if (inBounds(row, col, tup, width, height)):
 						if image[row + tup[0]][col + tup[1]] == 0:
 							isZero = True
@@ -40,105 +39,68 @@ def inBounds(row, col, tup, width, height):
 	return col+tup[1] >= 0 and row+tup[0] >= 0 and col+tup[1] < width and row+tup[0] < height
 
 
-def dilation(image, structure_element):
+def dilation(image, structureElement):
 	height = len(image)
 	width = len(image[0])
 
 	finArr = [[0 for x in xrange(width)] for x in xrange(height)]
-	flattened_elements= flatten_structure_element(structure_element)
+	flattenedElements= flattenStructureElement(structureElement)
 	for row in range(height):
 		for col in range(width):
 			pixel = image[row][col]
 			if pixel == 0:
 				isNotZero = False
-				for tup in flattened_elements:
-					#check bounds
+				for tup in flattenedElements:
 					if (inBounds(row, col, tup, width, height)):
 						if image[row + tup[0]][col + tup[1]] == 255:
 							isNotZero = True
 							finArr[row + tup[0]][col + tup[1]] = 255
 							break
 				if (not isNotZero):
-					finArr[row][col] = 255
+					finArr[row][col] = 0
 			else:
 				finArr[row][col] = 255
 	return finArr
-	# width, height = image.size
+	
+def opening(image, structureElement):
+	erodedArray = erosion(image, structureElement)
+	dilatedArray = dilation(erodedArray, structureElement)
+	return dilatedArray
 
-	# finArr = [[0 for x in xrange(width)] for x in xrange(height)]
-	# flattened_elements= flatten_structure_element(structure_element)
-	# for row in range(height):
-	# 	for col in range(width):
-	# 		pixel = image.getpixel((col, row))
-	# 		if pixel != 0:
-	# 			isZero = False
-	# 			for tup in flattened_elements:
-	# 				#check bounds
-	# 				if (inBounds(row, col, tup, width, height)):
-	# 					if image.getpixel((col + tup[1], row + tup[0]))[0] == 255:
-	# 						isZero = True
-	# 						finArr[row][col] = 1
-	# 						break
-	# 			if (not isZero):
-	# 				finArr[row][col] = 0
-	# return finArr
-
-def opening(image, structure_element):
-	eroded_array = erosion(image, structure_element)
-	dilated_array = dilation(eroded_array, structure_element)
-	return dilated_array
-
-def closing(image, structure_element):
-	dilated_array = dilation(image, structure_element)
-	eroded_array = erosion(dilated_array, structure_element)
-	return eroded_array
+def closing(image, structureElement):
+	dilatedArray = dilation(image, structureElement)
+	erodedArray = erosion(dilatedArray, structureElement)
+	return erodedArray
 
 def boundary(image):
 	height = len(image)
 	width = len(image[0])
 
-	structure_element = [[1, 1, 1], [1, 1, 1], [1, 1, 1]]
-	erodedImage = erosion(image, structure_element)
-	finArr = [[0 for x in xrange(width)] for x in xrange(height)]
+	structureElement = [[1, 1, 1], [1, 1, 1], [1, 1, 1]]
+	erodedImage = erosion(image, structureElement)
 	for row in range(height):
 		for col in range(width):
 			if erodedImage[row][col] == 255:
 				image[row][col] = 0
-			# finArr[row][col] = min(erodedImage[row][col], image[row][col])
-	# finArr = [[0 for x in xrange(width)] for x in xrange(height)]
-	# flattened_elements= flatten_structure_element(structure_element)
-	# for row in range(height):
-	# 	for col in range(width):
-	# 		pixel = image[row][col]
-	# 		if pixel == 0:
-	# 			isNotZero = False
-	# 			for tup in flattened_elements:
-	# 				#check bounds
-	# 				if (inBounds(row, col, tup, width, height)):
-	# 					if image[row + tup[0]][col + tup[1]] == 255:
-	# 						isNotZero = True
-	# 						image[row + tup[0]][col + tup[1]] = 255
-	# 						break
 	return image
 
-def flatten_structure_element(structure_element):
-	center = len(structure_element) / 2
-	flattened_structure_element = []
+def flattenStructureElement(structureElement):
+	center = len(structureElement) / 2
+	flattenedStructureElement = []
 
-	for row in range(len(structure_element)):
-		for col in range(len(structure_element[row])):
-			if structure_element[row][col] == 1:
-				flattened_structure_element.append((row - center, col - center))
+	for row in range(len(structureElement)):
+		for col in range(len(structureElement[row])):
+			if structureElement[row][col] == 1:
+				flattenedStructureElement.append((row - center, col - center))
 
-	return flattened_structure_element
+	return flattenedStructureElement
 
 
-image = read_image('test_erosion_2.bmp')
-# print image
-structure_element = [[1, 1, 1], [1, 1, 1], [1, 1, 1]]
-# print flatten_structure_element(structure_element)
-finalArr = boundary(image)
-# print finalArr
+# TESTING
+
+image = readImage('gun.bmp')
+structureElement = [[1, 1, 1], [1, 1, 1], [1, 1, 1]]
+finalArr = opening(image, structureElement)
 numpyArr = (np.array(finalArr)).astype(np.uint8)
 im = Image.fromarray(numpyArr)
-im.save('result_closing.bmp')
+im.save('resultOpeningGun3x3.bmp')
