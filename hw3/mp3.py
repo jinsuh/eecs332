f5e87760239156c12e6857226ce5b424070335c1
GregChan <gregorywarrenchan@gmail.com>
2014-10-18 00:28:01 -0500

    almost working code

--- hw3/mp3.py
+++ hw3/mp3.py
@@ -9,18 +9,51 @@ def readImage(imagePath):
 
 def histogram(imageData):
 	width, height = imageData.size
-	hist = [0 for x in range(255)]
-	for row in range(width):
-		for col in range(height):
-			pixel = imageData.get((col, row))
-			hist[pixel] = hist[pixel] + 1
-	return hist
+	histogram = [0 for x in range(256)]
+	for row in range(height):
+		for col in range(width):
+			pixel = imageData.getpixel((col, row))
+			rgb_average = (pixel[0] + pixel[1] + pixel[2]) / 3
+			histogram[rgb_average] = histogram[rgb_average] + 1
+	return histogram
 
-def probabilityMassFunction(histogram_data, size):
-	for i in range(len(histogram_data)):
-		value = histogram_data[i]
-		histogram_data[i] = value / size
+def probabilityMassFunction(histogramData, size):
+	for i in range(len(histogramData)):
+		value = histogramData[i]
+		histogramData[i] = value / float(size)
 
-	return histogram_data
+	return histogramData
 
+def cumulativeDistributiveFunction(histogramData):
+	for i in range(len(histogramData)):
+		value = histogramData[i]
 
+		if i > 0:
+			histogramData[i] = histogramData[i] + histogramData[i - 1]
+
+	return histogramData 
+
+def histogramEqualization(imageData, histogramData):
+	width, height = imageData.size
+	image = [[0 for x in xrange(height)] for x in xrange(width)]
+	for row in range(height):
+		for col in range(width):
+			pixel = imageData.getpixel((col, row))
+			rgb_average = (pixel[0] + pixel[1] + pixel[2]) / 3
+			image[col][row] = histogramData[rgb_average] * 256
+
+	return image
+
+def createResultImage(imageArray, name):
+	array = (np.array(imageArray)).astype(np.uint8)
+	image = Image.fromarray(array)
+	image.save(name + '.bmp', 'bmp')
+
+#test code
+image = readImage('moon.bmp')
+width, height = image.size
+histogramData = histogram(image)
+histogramData = probabilityMassFunction(histogramData, width * height)
+histogramData = cumulativeDistributiveFunction(histogramData)
+newImage = histogramEqualization(image, histogramData)
+createResultImage(newImage, 'moon_result')
