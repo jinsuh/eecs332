fa148a5d2c21d45685b13abc153574aaed613113
GregChan <gregorywarrenchan@gmail.com>
2014-12-10 18:28:32 -0600

    working?

--- project/video_tracking.m
+++ project/video_tracking.m
@@ -2,10 +2,6 @@ function main(frames, nFrames)
 	H = zeros(1);
 	[height, width] = size(frames(:, :, 1, 1));
 
-	imwrite(frames(13:75, 13:75, :, 1), 'test.bmp');
-	text_synth('test.bmp', 50, 240, 320);
-	synthesizedTexture = imread('res_block.jpg');
-
 	% generate average
 	for i=1:(int32(nFrames)/10 - 1)
 		fprintf('frame %d\n', i * 10);
@@ -43,6 +39,44 @@ function main(frames, nFrames)
 	vid.FrameRate = 15;
 	open(vid);
 
+	% line = VideoWriter('line.avi');
+	% line.FrameRate = 15;
+	% open(line);
+
+	count = 0;
+	right = [zeros(height, width); zeros(height, width); zeros(height, width)];
+
+	prevCol = -1;
+	
+	frame = frames(:, :, :, 1);
+
+	redTopRow = 0;
+	redBottomRow = 0;
+
+	for r = 1:height
+		for c = 1:width
+			if isRed(frame, r, c) == 1
+				redTopRow = r;
+				r = height + 1;
+				break
+			end
+		end
+	end
+
+	for r = height:-1:1
+		for c = 1:width
+			if isRed(frame, r, c) == 1
+				imwrite(frame(r,c,:), 'frame.bmp');
+				redBottomRow = r;
+				r = 0;
+				break
+			end
+		end
+	end
+
+	redTopRow = 60;%redTopRow - 5
+	redBottomRow = 160;%redBottomRow + 5
+
 	for i=1:nFrames
 		fprintf('frame %d\n', i);
 		frame = int32((frames(:, :, 1, i) + frames(:, :, 2, i) + frames(:, :, 3, i)) / 3);
@@ -51,27 +85,58 @@ function main(frames, nFrames)
 		binary_image = generateBinaryImage(difference, 60);
 		[row, col] = centroid(binary_image);
 		
-		frame = frames(:, :, :, i);
-		% frame = drawLine(frame, col, 0, synthesizedTexture);
-
-		for r = 60:130
-			for c = col:width - 30
-				% if frame(r, c, 1) > 120 && frame(r, c, 2) < 120 && frame(r, c, 3) < 120
-				frame(r, c, 1) = 0;
-				frame(r, c, 2) = 0;
-				frame(r, c, 3) = 255;
-				% end
-			end
+		if prevCol == -1
+			prevCol = col;
 		end
 
+		frame = frames(:, :, :, i);
+
+		% frame = drawLine(frame, col, 0, synthesizedTexture);
+		
+		if mod(count, 5) == 0
+			imwrite(frames(redBottomRow:redBottomRow+18, col:col+18, :, 1), 'test.bmp');
+			text_synth('test.bmp', 18, abs(redBottomRow - redTopRow), 40);
+			synthesizedTexture = imread('res_block.jpg');
+			for r = 1:abs(redBottomRow - redTopRow)
+				for c = 1:(prevCol - col) + 4
+					if isRed(frame, r, c) == 1
+						% frame(r + 60, c + col, 1) = synthesizedTexture(r, c, 1);
+						% frame(r + 60, c + col, 2) = synthesizedTexture(r, c, 2);
+						% frame(r + 60, c + col, 3) = synthesizedTexture(r, c, 3);
+						right(r + redTopRow, c + col, 1) = synthesizedTexture(r, c, 1);
+						right(r + redTopRow, c + col, 2) = synthesizedTexture(r, c, 2);
+						right(r + redTopRow, c + col, 3) = synthesizedTexture(r, c, 3);
+					end
+				end
+			end
+			
+			for r = 1:height
+				for c = 1:width
+					if right(r, c, 1) ~= 0 && right(r, c, 2) ~= 0 && right(r, c, 3) ~= 0
+						frame(r, c, 1) = right(r, c, 1);
+						frame(r, c, 2) = right(r, c, 2);
+						frame(r, c, 3) = right(r, c, 3);
+					end
+				end
+			end
+		end
+		prevCol = col;
+		% lineFrame = zeros(size(frame));
+		% lineFrame = drawLine(lineFrame, col, 0);
 		% fileName = sprintf('frame_%d.bmp',i);
 		% imwrite(frame, fileName);
+		% writeVideo(line, uint8(lineFrame));
 		writeVideo(vid, frame);
+		cout = count + 1;
 	end
 	close(vid);
 end
 
-function image = drawLine(image, r, theta, synthesizedTexture)
+function value = isRed(frame, r , c)
+	value = (frame(r, c, 1) > 50 && frame(r, c, 2) < 150 && frame(r, c, 3) < 120);
+end
+
+function image = drawLine(image, r, theta)
 	[height, width] = size(image);
 
 	for row = 1:height
@@ -80,7 +145,6 @@ function image = drawLine(image, r, theta, synthesizedTexture)
 				image(row, col, 1) = 255;
 				image(row, col, 2) = 0;
 				image(row, col, 3) = 0;
-				% frame(row, col, :)
 			end
 		end
 	end
